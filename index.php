4593a105ce398c7ca4f791771e0c882d13fa8e25
Oscar Martínez <99136661+OscarTambito@users.noreply.github.com>
2022-05-09 19:48:08 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Practica #1 | IA</title>
      <!-- fontawesome -->
      <script src="https://kit.fontawesome.com/42e7f98aa1.js" crossorigin="anonymous"></script>
+     <script src="js/holo.js"></script>
 
     <link rel="stylesheet" href="css/bot.css">
 </head>
@@ -14,17 +15,17 @@
     <div id="container">
         <div id="dot1"></div>
         <div id="dot2"></div>
-        <div id="chatBtn"> <button id="botonChat"><img src="./img/chat.png" alt=""></button> </div>
+        <div id="chatBtn"> <button id="botonChat" onclick="iniciar()"><img src="./img/chat.png" alt=""></button> </div>
         <div id="screen">
             <div id="header">
                 <div id="titulo">
                     JACK UVG - soporte 
                 </div>
                 <div id="actualizar">
-                    <button id="botonAct"><img src="./img/refresh.png" alt=""></button>
+                    <button id="botonAct" onclick="iniciar()"><img src="./img/refresh.png" alt=""></button>
                 </div>
                 <div id="cerrar">
-                    <button id="botonCerrar"><img src="./img/close.png" alt=""></button>
+                    <button id="botonCerrar" onclick="salir()"><img src="./img/close.png" alt=""></button>
                 </div>
              
             </div>
@@ -37,9 +38,9 @@
                 2. Selecciona alguno de los temas que aparecen a continuación:
                 </div>
                 <div class="options">
-                    <input type="submit" id="carrera" value="Carreras">
-                    <input type="submit" id="admision" value="Admisiones">
-                    <input type="submit" id="servicio" value="Servicios">
+                    <input type="submit" id="carrera" value="Carreras" onclick="carrera1()">
+                    <input type="submit" id="admision" value="Admisiones" onclick="carrera2()">
+                    <input type="submit" id="servicio" value="Servicios" onclick="carrera3()">
                 </div>
                 <!--Mensajes del usuario-->
 
@@ -47,7 +48,7 @@
             <!-- messages input field -->
             <div id="userInput">
                 <input type="text" name="messages" id="messages" autocomplete="OFF" placeholder="Escribe tu mensaje." required>
-                <input type="submit" value="Enviar" id="send" name="send">
+                <input type="submit" value="Enviar" id="send" name="send" onclick="enviar()">
             </div>
         </div>
     </div>
@@ -57,5 +58,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     
     <script src="./js/main.js"></script>
+
+    <div id="holograma">
+    <video   autoplay src="img/video.mp4" width="1500" height="650" id="video"> </video>
+    </div>
 </body>
 </html>
